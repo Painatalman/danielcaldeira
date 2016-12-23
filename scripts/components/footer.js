8c8d0c91da7b5384527b952691cc5cc414fd56a9
Miguel Costa <maiquedi@gmail.com>
2016-12-23 23:55:07 +0000

    update scripts overall and adjust footer credits

--- scripts/components/footer.js
+++ scripts/components/footer.js
@@ -5,10 +5,10 @@ const Footer = () => (
     <div className="row l-footer__row">
       <div className="col-sm-6 l-footer__col">
         <div className="footer__credits">
-          <div className="col-xs-4 l-footer__col footer__credit">
+          <div className="col-sm-6 l-footer__col footer__credit">
             <spam className="footer__credit-title">Design</spam>: Daniel Caldeira
           </div>
-          <div className="col-xs-6 l-footer__col footer__credit">
+          <div className="col-sm-6 l-footer__col footer__credit">
             <spam className="footer__credit-title">Desenvolvimento</spam>: Miguel Costa
           </div>
         </div>
